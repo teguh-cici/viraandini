068f3d7dd316421b0f1ca82bb69fd82ce1fc71d7
teguh-cici <iyorizki76@gmail.com>
2025-10-19 18:02:40 +0700

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,22 @@
+import bg from './bg.jpg'
+
+import picture from './picture.png'
+import picture2 from './picture2.png'
+import picture3 from './picture3.png'
+import picture4 from './picture4.png'
+
+import confetti from './confetti.gif'
+
+import CakeSVG from './svg/CakeSVG'
+
+
+
+export {
+    bg,
+    picture,
+    picture2,
+    picture3,
+    picture4,
+    confetti,
+    CakeSVG
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,86 @@
+import { motion } from "framer-motion";
+import React, { useRef, useState } from "react";
+import "../assets/css/card.css";
+import { Link } from 'react-router-dom'; 
+
+function Card() {
+  const [cardClass, setCardClass] = useState("");
+  const [isCardOpened, setIsCardOpened] = useState(false);
+  const timerRef = useRef(null);
+
+  const toggleCard = () => {
+    if (cardClass === "" || cardClass === "close-half") {
+      setCardClass("open-half");
+      setIsCardOpened(true); 
+      if (timerRef.current) clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        setCardClass("open-fully");
+        timerRef.current = null;
+      }, 1000);
+    } else {
+      setCardClass("close-half");
+      if (timerRef.current) clearTimeout(timerRef.current);
+      timerRef.current = setTimeout(() => {
+        setCardClass("");
+        timerRef.current = null;
+      }, 1000);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center overflow-clip">
+    <div className="w-[400px]  h-screen flex flex-col items-center justify-center">
+      <motion.div  initial={{ opacity: 0, visibility: "hidden" }}
+          animate={{ opacity: 1, visibility: "visible" }}
+          transition={{duration: 1.2}}>
+      <div id="card" className={`${cardClass}`} onClick={toggleCard}>     
+        <div id="card-inside">
+          <div className="wrap">
+            <p>happy birthday, vira andini!</p>
+            <p>hari ini adalah hari istimewamu, semoga segala kebahagiaan dan keceriaan menyertai setiap langkahmu di usia yang baru ini.</p>
+            <p>
+              Pesan: <br />
+              Terima kasih selama ini udah nemenin aku, semoga kedepannya vira bahagia terus walau udah gaada aku lagi.
+            </p>
+            <p>
+             Jangan lupain aku ya vira, aku masih ada disini kalau vira butuh apa-apa.
+            </p>
+            <p>
+              abang sayang vira ❤
+            </p>
+            <p className="signed">iyo.-</p>
+          </div>
+        </div>
+
+        <div id="card-front">
+          <div className="wrap">
+            <h1>Happy Birthday!</h1>
+          </div>
+        </div>
+    </div>
+
+      </motion.div>
+
+    {/* prone to bugs */}
+      {isCardOpened && (
+        <motion.div className="-mt-[3rem]" initial={{ opacity: 0, visibility: "hidden" }}
+        animate={{ opacity: 1, visibility: "visible" }}
+        transition={{duration: 1.2}}> 
+        <Link to ='/cake'>
+        <p className="-mt-[4rem] px-7 py-3 bg-customBlue text-white font-medium text-base rounded-full hover:bg-blue-600">
+            Halaman Berikutnya
+          </p>
+        </Link>
+            
+        </motion.div>
+         
+        )}
+
+    </div>
+    
+    </div>
+    
+  );
+}
+
+export default Card;
